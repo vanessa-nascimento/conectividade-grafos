e7a47e463dfd8e7a3f404fd2cc4fc9c2b89238c1
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2021-06-18 20:56:07 -0300

    Atualizando componentes conexas

--- componenteConexa.c
+++ componenteConexa.c
@@ -2,33 +2,38 @@
 #include <stdlib.h> 
 #include "grafo.h"
 
-void componenteConexa(Grafo * grafo){
-  int id = 0;
-  int componenteConexo[grafo->numVertices];
-
-  for (int vertice = 0; vertice < grafo->numVertices; ++vertice) {
-    componenteConexo[vertice] = -1;
+void inicializaComponenteConexa(Grafo * grafo, int componente[]) {
+   for (int vertice = 0; vertice < grafo->numVertices; ++vertice) {
+    componente[vertice] = -1;
   }
+}
+
+void componenteConexa(Grafo * grafo) {
+  int componente[grafo->numVertices];
+  int idComponente = 0;
+  
+ inicializaComponenteConexa(Grafo * grafo, componente[grafo->numVertices]);
   
   fprintf(stdout, "\nComponentes Conexos:");
   for (int vertice = 0; vertice < grafo->numVertices; ++vertice) {
-    if (componenteConexo[vertice] == -1) {
-      id++;
-      fprintf(stdout, "\nC%d: ", id);
-      verificaVerticeComponenteConexo(grafo, componenteConexo, vertice, id);
+    if (componente[vertice] == ANT_INICIO) {
+      idComponente++;
+      fprintf(stdout, "\nC%d: ", idComponente);
+      verificaVerticeComponenteConexo(grafo, vertice, componente, idComponente);
     }
   }
   fprintf(stdout, "\n");
 }
 
-void verificaVerticeComponenteConexo(Grafo * grafo, int componenteConexo[], int vertice, int idComponente) {
-  componenteConexo[vertice] = idComponente;
-  fprintf(stdout, "%d ", vertice);
+void verificaVerticeComponenteConexo(Grafo * grafo, int vertice, int componente[], int idComponente) {
   Apontador atual = grafo->listaAdj[vertice];
+  componente[vertice] = idComponente;
+
+  fprintf(stdout, "%d ", vertice);
 
   while (atual != NULL) {
-    if(componenteConexo[atual->vdest] == -1) {
-      verificaVerticeComponenteConexo(grafo, componenteConexo, atual->vdest, idComponente);
+    if(componente[atual->vdest] == ANT_INICIO) {
+      verificaVerticeComponenteConexo(grafo, componente, atual->vdest, idComponente);
     }
     atual = atual->prox;
   }
